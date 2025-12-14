5d09f7782b3d04b9b8f69753d4b7625d676a4370
Gyeonghun Min <riverpower6@naver.com>
2025-12-14 20:35:33 +0900

    [FEAT] 상품 메인 페이지 일부 구현
    1. 일부 API 호출 자바스크립트 작성
    2. 카테고리 선택 시 실시간 렌더링
      - 선택된 카테고리 아래에 선택할 수 있는 하위 카테고리 리스트 추가 렌더링
    3. 검색창 추가
    4. 메인 페이지 UI 추가
    5. 정렬 관련 정보 추가
    6. 가격 범위 추가 (작게 태그 형식으로 수정할 것으로 예상)

--- src/api/index.js
+++ src/api/index.js
@@ -4,16 +4,15 @@ const openApi = axios.create({
   baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
-  }
+  },
 })
 
 const api = axios.create({
-    baseURL: '/api',
-    headers: {
-      'X-CODE': sessionStorage.getItem('X-CODE'),
-      'Content-Type': 'application/json',
-    }
-  }
-)
+  baseURL: '/api',
+  headers: {
+    'X-CODE': sessionStorage.getItem('X-CODE'),
+    'Content-Type': 'application/json',
+  },
+})
 
 export { api, openApi }
